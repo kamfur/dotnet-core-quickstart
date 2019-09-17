b6f6af8d2f23bad62b2fd8d21b0f9cc8c9101f5c
Kamil Furtak <kamil.furtak@dieboldnixdorf.com>
2019-09-17 16:57:49 +0200

    strucutre and component view

--- structurizr/Program.cs
+++ structurizr/Program.cs
@@ -11,43 +11,115 @@ namespace structurizr
     class Program
     {
 
-        private const long WorkspaceId = 1234;
-        private const string ApiKey = "key";
-        private const string ApiSecret = "secret";
+        private const long WorkspaceId = 46661;
+        private const string ApiKey = "4e6ff25c-a4fa-4e4f-9cb2-3633bac26323";
+        private const string ApiSecret = "caa32198-39a5-4965-8988-84581ada4622";
 
+        
+        static void AddContext(Model model1, ViewSet viewSet)
+        {
+           
+        }
         static void Main(string[] args)
         {
-            // a Structurizr workspace is the wrapper for a software architecture model, views and documentation
-            Workspace workspace = new Workspace("Getting Started", "This is a model of my software system.");
+            
+
+            Workspace workspace = new Workspace("Park Soft", "Software for parking system");
             Model model = workspace.Model;
-
-            // add some elements to your software architecture model
-            Person user = model.AddPerson("User", "A user of my software system.");
-            SoftwareSystem softwareSystem = model.AddSoftwareSystem("Software System", "My software system.");
-            user.Uses(softwareSystem, "Uses");
-
-            // define some views (the diagrams you would like to see)
             ViewSet views = workspace.Views;
-            SystemContextView contextView = views.CreateSystemContextView(softwareSystem, "SystemContext",
-                "An example of a System Context diagram.");
-            contextView.PaperSize = PaperSize.A5_Landscape;
-            contextView.AddAllSoftwareSystems();
-            contextView.AddAllPeople();
+            
+            AddContext(model, views);
+            model.Enterprise = new Enterprise("System parkingowy");
+            Person customer = model.AddPerson(
+                Location.External,
+                "Klient",
+                "Klient parkingu, " +
+                "posiada konto w systemie");
+            
+            Person parkingOperator = model.AddPerson(
+                Location.External,
+                "Operator",
+                "Operator parkingu, " +
+                "posiada konto w systemie");
 
-            // add some documentation
-            StructurizrDocumentationTemplate template = new StructurizrDocumentationTemplate(workspace);
-            template.AddContextSection(softwareSystem, Format.Markdown,
-                "Here is some context about the software system...\n" +
-                "\n" +
-                "![](embed:SystemContext)");
+            SoftwareSystem clientPlatformSystem = model.AddSoftwareSystem(
+                Location.Internal,
+                "Platforma kliencka",
+                "Pozwala na zarządzanie swoimi rezerwacjami");
+            customer.Uses(clientPlatformSystem, "Używa");
 
-            // add some styling
-            Styles styles = views.Configuration.Styles;
-            styles.Add(new ElementStyle(Tags.SoftwareSystem) { Background = "#1168bd", Color = "#ffffff" });
-            styles.Add(new ElementStyle(Tags.Person) { Background = "#08427b", Color = "#ffffff", Shape = Shape.Person });
+            SoftwareSystem mainframeParkingSystem = model.AddSoftwareSystem(
+                Location.Internal,
+                "System parkingowy",
+                "Core'owy system parkingowy.");
+            
+            clientPlatformSystem.Uses(
+                mainframeParkingSystem,
+                "Pobiera informacje/Wysyła informacje");
+            parkingOperator.Uses(mainframeParkingSystem, "obsługuje");
+            
+            SoftwareSystem emailSystem = model.AddSoftwareSystem(Location.Internal,
+            "E-mail System", "System e-mailowy Microsoft Exchange");
+            clientPlatformSystem.Uses(emailSystem, "Wysyła e-maile używając");
+            emailSystem.Delivers(customer, "Wysyła e-maile do");
+            clientPlatformSystem.Uses(
+                mainframeParkingSystem,
+                "Pobiera informacje/Wysyła informacje");
+
+            
+            
+            SystemContextView systemContextView = views.CreateSystemContextView(
+                clientPlatformSystem,
+                "SystemContext",
+                "Diagram kontekstowy systemu parkingowego");
+            systemContextView.AddNearestNeighbours(clientPlatformSystem);
+            systemContextView.AddAnimation(clientPlatformSystem);
+            systemContextView.AddAnimation(customer);
+            //systemContextView.AddAnimation(parkingOperator);
+            systemContextView.AddAnimation(mainframeParkingSystem);
+            
+            //wcześniej zdefiniowany kontekst
+            Container mobileApp = mainframeParkingSystem.AddContainer(
+                "Aplikacja mobilna",
+                "Dostarcza ograniczoną funkcjonalność bankowości online dla klienta",
+            "Xamarin");
+            Container apiApplication = mainframeParkingSystem.AddContainer(
+                "API",
+                "Dostarcza funkcjonalność bankowości online poprzez JSON/HTTP",
+            "Java i Spring MVC");
+            Container database = mainframeParkingSystem.AddContainer(
+                "Baza danych", "Informacje o klientach, hashowane hasła, logi",
+            "Relacyjna baza danych");
+            parkingOperator.Uses(mobileApp, "Używa", "");
+            apiApplication.Uses(database,
+                "Czyta/Zapisuje",
+                "JDBC");
+            apiApplication.Uses(mainframeParkingSystem,
+                "Używa",
+                "XML/HTTPS");
+            apiApplication.Uses(emailSystem, "Wysyła maile", "SMTP");
+            
+            
+            ContainerView containerView = views.CreateContainerView(
+                clientPlatformSystem,
+                "Containers",
+                "Diagram kontenerów systemu Platformy Bankowowości Online");
+            containerView.Add(customer);
+            containerView.AddAllContainers();
+            containerView.Add(mainframeParkingSystem);
+            containerView.Add(emailSystem);
+            containerView.AddAnimation(
+                customer,
+                mainframeParkingSystem,
+                emailSystem);
+//            containerView.AddAnimation(mobileApp);
+//            containerView.AddAnimation(apiApplication);
+
 
             UploadWorkspaceToStructurizr(workspace);
         }
+
+   
 
         private static void UploadWorkspaceToStructurizr(Workspace workspace) {
             StructurizrClient structurizrClient = new StructurizrClient(ApiKey, ApiSecret);
